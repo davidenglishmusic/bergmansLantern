6466d15eb5079412f1c0e58a2f95cc630e991a46
David English <david@operaonfilm.com>
2014-08-25 09:43:32 -0700

    Adjusted positioning CSS so that the slideshow will push content down instead of overlapping it

--- js/lantern.js
+++ js/lantern.js
@@ -12,7 +12,7 @@ lanternSlideshow.prototype.getLanternSlidesCode = function(imageArray) {
     if (imageArray[j][1].length < 1) {
       lanternFieldSetCode = lanternFieldSetCode + '<div class="lanternSlide" id="slide' + (j+1) + '"><img src="' + imageArray[j] + '" ></div>';
     }
-    else { 
+    else {
       lanternFieldSetCode = lanternFieldSetCode + '<div class="lanternSlide" id="slide' + (j+1) + '"><a href="' + imageArray[j][1] + '"><img src="' + imageArray[j] + '" ></a></div>';
 
     }
@@ -23,7 +23,7 @@ lanternSlideshow.prototype.getLanternSlidesCode = function(imageArray) {
 lanternSlideshow.prototype.getLanternJSCode = function(imageArray, lanternSpeed) {
   var jquerySlideLines = '<script> function lanternGo() {';
   for (var k=0; k < imageArray.length; k++) {
-    jquerySlideLines = jquerySlideLines + '$("#slide' + (k+1) + '").fadeIn( ' + lanternSpeed[1] + ' ).delay( ' + lanternSpeed[0] + ' ).fadeOut( ' + lanternSpeed[1] + ', function() {'; 
+    jquerySlideLines = jquerySlideLines + '$("#slide' + (k+1) + '").fadeIn( ' + lanternSpeed[1] + ' ).delay( ' + lanternSpeed[0] + ' ).fadeOut( ' + lanternSpeed[1] + ', function() {';
   }
   var jquerySlideLineClosingTags = '';
   for (var q=0; q < imageArray.length; q++) {
@@ -36,15 +36,14 @@ lanternSlideshow.prototype.getLanternJSCode = function(imageArray, lanternSpeed)
 lanternSlideshow.prototype.getEmbedCode = function(imageArray, lanternWidth, lanternSpeed) {
 
   var lanternContainerCode = '<!--beginLanternCode--><div id="lanternSlideContainer">';
-  
+
   var lanternSlidesCode = this.getLanternSlidesCode(imageArray) + '</div>';
 
   var lanternCodeToRunSlideshow = '$( document ).ready(function() { setTimeout(function() {lanternGo(); },1500); });</script>';
 
   var lanternJSCode = this.getLanternJSCode(imageArray, lanternSpeed) + lanternCodeToRunSlideshow;
 
-  var lanternStyleCode = '<style> #lanternSlideContainer {width:' + lanternWidth + ';position:relative;margin-left:auto;margin-right:auto;} .lanternSlide {position:absolute;display:none;} .lanternSlide img{width:100%;} .lanternSlide a:active {outline: none;} </style><!--endLanternCode-->';
+  var lanternStyleCode = '<style> #lanternSlideContainer {width:' + lanternWidth + ';position:relative;margin-left:auto;margin-right:auto;} .lanternSlide {position:relative;display:none;} .lanternSlide img{width:100%;} .lanternSlide a:active {outline: none;} </style><!--endLanternCode-->';
 
   return lanternContainerCode + lanternSlidesCode + lanternJSCode + lanternStyleCode;
 }
-
